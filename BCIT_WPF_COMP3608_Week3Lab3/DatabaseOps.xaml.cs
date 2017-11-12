ba9f4a6ec14fb336b84619b65dacc6b3c81582fa
kszczurowski <kszczurowski@absolute.com>
2017-11-12 11:03:04 -0800

    Initial commit for Week 3 Lab 3 solution. This is a vanila version based on finished Lab 2.

---
+++ BCIT_WPF_COMP3608_Week3Lab3/DatabaseOps.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using DataAccess;
+using DataAccess.Northwind2013DataSetTableAdapters;
+
+namespace BCIT_WPF_COMP3608_Week3Lab3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class DatabaseOps : Window
+    {
+        Northwind2013DataSet aset = new Northwind2013DataSet();
+        CustomersTableAdapter custAdap = new CustomersTableAdapter();
+        OrdersTableAdapter ordAdap = new OrdersTableAdapter();
+        Order_Details_ExtendedTableAdapter ord_det_extAdap = new Order_Details_ExtendedTableAdapter();
+
+        public DatabaseOps()
+        {
+            InitializeComponent();
+            custAdap.Fill(aset.Customers);
+            ordAdap.Fill(aset.Orders);
+            ord_det_extAdap.Fill(aset.Order_Details_Extended);
+            Grid1.DataContext = aset.Customers;
+        }
+    }
+}
